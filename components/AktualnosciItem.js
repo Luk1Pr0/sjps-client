c8d9cc32c5171e6c64a1dc79dfe0f6856b694889
luk1pr0 <lpytel16@gmail.com>
2022-05-14 00:55:50 +0100

    Change to the aktualnosci component styling and content

--- components/AktualnosciItem.js
+++ components/AktualnosciItem.js
@@ -3,29 +3,23 @@ import Link from 'next/link';
 export default function Aktualnosci() {
 	return (
 		<>
-			<section className="content-wrapper content-wrapper--center">
+			<aside className="aktualnosci-item-wrapper">
 
-				<div className="aktualnosci-item-wrapper">
+				<h5>Drodzy Rodzice / Opiekunowie, Przyjaciele naszej szkoły!</h5>
 
-					<strong><h2>Aktualności</h2></strong>
+				<br />
 
-					<h5>Drodzy Rodzice / Opiekunowie, Przyjaciele naszej szkoły!</h5>
+				<p>
+					Z dniem <strong>30 kwietnia</strong> rozpoczęliśmy zapisy uczniów na rok szkolny <strong>2022/2023</strong>. <Link href='/zapisy'><a className="link">Formularz rejestracyjny</a></Link> jest dostępny na stronie internetowej naszej szkoły. Prosimy o przekazanie informacji o zapisach rodzinie i znajomym pragnącym zapisać dziecko/dzieci do naszej szkoły.
+				</p>
 
-					<br />
+				<br />
 
-					<p>
-						Z dniem <strong>30 kwietnia</strong> rozpoczęliśmy zapisy uczniów na rok szkolny <strong>2022/2023</strong>. <Link href='/zapisy'><a className="link">Formularz rejestracyjny</a></Link> jest dostępny na stronie internetowej naszej szkoły. Prosimy o przekazanie informacji o zapisach rodzinie i znajomym pragnącym zapisać dziecko/dzieci do naszej szkoły.
-					</p>
+				<p>
+					Pytania dotyczące zapisów prosimy kierować na adres email: <span><a href="mailto:szkolajezykapolskiegoslough@gmail.com?subject=Formularz rejestracyjny" className="link">szkolajezykapolskiegoslough@gmail.com</a></span>. Nasz szkolny numer telefonu jest również do Państwa dyspozycji od <strong>poniedziałku</strong> do <strong>piątku</strong> w godz. od <strong>17:30-20:00</strong>. Zapraszamy serdecznie!
+				</p>
 
-					<br />
-
-					<p>
-						Pytania dotyczące zapisów prosimy kierować na adres email: <span><a href="mailto:szkolajezykapolskiegoslough@gmail.com?subject=Formularz rejestracyjny" className="link">szkolajezykapolskiegoslough@gmail.com</a></span>. Nasz szkolny numer telefonu jest również do Państwa dyspozycji od <strong>poniedziałku</strong> do <strong>piątku</strong> w godz. od <strong>17:30-20:00</strong>. Zapraszamy serdecznie!
-					</p>
-
-				</div>
-
-			</section>
+			</aside>
 		</>
 	)
 }
--- components/AktualnosciList.js
+++ components/AktualnosciList.js
@@ -2,8 +2,12 @@ import AktualnosciItem from "./AktualnosciItem";
 
 export default function AktualnosciList() {
 	return (
-		<>
+		<section className="content-wrapper content-wrapper--center">
+
+			<strong><h2>Aktualności</h2></strong>
+
 			<AktualnosciItem />
-		</>
+
+		</section>
 	)
 }
