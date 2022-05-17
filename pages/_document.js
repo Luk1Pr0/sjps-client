3b509ebc4ed2c2f5f77a6a7f78ebbf07c5482e15
luk1pr0 <lpytel16@gmail.com>
2022-05-17 22:37:54 +0100

    Change for SEO, added lang to the documentJS file

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class CustomDocument extends Document {
 	render() {
 		return (
-			<Html>
+			<Html lang='pl'>
 				<Head>
 					<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
 
