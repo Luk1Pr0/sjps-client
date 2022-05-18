17af9a8c585665af8cc358d24b2c168533e361ca
luk1pr0 <lpytel16@gmail.com>
2022-05-18 23:37:55 +0100

    Added a loader for aktualnosci list

--- components/AktualnosciList.js
+++ components/AktualnosciList.js
@@ -2,13 +2,20 @@ import { useEffect, useState } from 'react';
 
 // COMPONENTS
 import AktualnosciItem from "./AktualnosciItem";
+import Loader from '../components/Loader';
 
 export default function AktualnosciList({ }) {
 
+	// STATE
 	const [updates, setUpdates] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
+	// FETCH UPDATES FROM THE SERVER
 	const getUpdates = async () => {
+		setIsLoading(true);
 		try {
+			// SHOW LOADER
+
 			// FETCH UPDATES FROM SERVER
 			const response = await fetch('https://sjps-server.herokuapp.com/aktualnosci', {
 				method: 'GET',
@@ -21,12 +28,10 @@ export default function AktualnosciList({ }) {
 			// CONVERT FROM JSON TO READABLE
 			const data = await response.json();
 
-
 			// SORT RETRIEVE UPDATES BY DATE DESCENDING - NEWEST FIRST
 			const sortedUpdates = data.sort((a, b) => {
 				return Date.parse(b.dateAdded) - Date.parse(a.dateAdded);
 			})
-
 
 			// SET STATE OF UPDATES
 			setUpdates(sortedUpdates);
@@ -34,8 +39,11 @@ export default function AktualnosciList({ }) {
 		} catch (error) {
 			return 'Error fetching updates from the server';
 		}
+		// SHOW LOADER
+		setIsLoading(false);
 	}
 
+	// ON RENDER
 	useEffect(() => {
 		getUpdates();
 	}, [])
@@ -47,13 +55,19 @@ export default function AktualnosciList({ }) {
 
 			<div className="aktualnosci-list-wrapper">
 
+				{/* WHEN CONTENT IS LOADING SHOW LOADER */}
+				{isLoading && <Loader />}
+
 				{
 					!updates.length ?
-						<h5>Brak aktualności</h5>
+						<section className="content-wrapper content-wrapper--half-screen">
+							<h5>Brak aktualności</h5>
+						</section>
 						:
 						updates.map((update) => (
 							<AktualnosciItem key={update._id} updateId={update._id} title={update.title} message={update.message} dateAdded={update.dateAdded} />
 						))
+
 				}
 
 			</div>
