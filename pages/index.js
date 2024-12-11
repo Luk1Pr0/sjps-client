3f38c45c72e51703cc597ee41b65c9fa3af40e18
luk1pr0 <lpytel16@gmail.com>
2024-12-11 23:02:10 +0000

    Added required text for compliance

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Card from '../components/Card';
 import DokumentyImg from '../public/index/dokumenty-img.svg';
 import KalendarzImg from '../public/index/kalendarz-img.svg';
 import ZapisyImg from '../public/index/zapisy-img.svg';
+import IRJPImg from '../public/index/irjp.jpg';
 import AktualnosciList from '../components/AktualnosciList';
 
 export default function Home() {
@@ -65,7 +66,18 @@ export default function Home() {
 
 			<main className='main main--index'>
 
-				<AktualnosciList />
+				<section className='content-wrapper'>
+					Z przyjemnoscia informujemy, że nasza szkoła Szkoła Języka Polskiego im. św.  Siostry Faustyny w Slough otrzymała za pośrednictwem Stowarzyszenia „Wspólnota Polska” dofinansowanie od Instytut Rozwoju Języka Polskiego Instytutu Rozwoju Języka Polskiego im. Św. Maksymiliana Marii Kolbego ze środków Ministra Edukacji Narodowej w ramach konkursu „Wsparcie nauczania języka polskiego za granicą poprzez dofinansowanie zakupu podręczników, sprzętu biurowego, sprzętu komputerowego oraz oprogramowania
+					Nazwa zadania publicznego: Wsparcie nauczania języka polskiego za granicą szkół na terenie Wielkiej Brytanii (1)
+					Kwota dotacji w 2024 r. :  33 700 PLN
+					Całkowita wartość zadania publicznego:  461 833,33 PL.
+					Zadanie publiczne „Wsparcie nauczania języka polskiego za granicą szkół na terenie Wielkiej Brytanii”  dotyczy dofinansowania kosztów zakupu podręczników, sprzętu biurowego, sprzętu komputerowego oraz oprogramowania.
+					
+					<img src={IRJPImg.src} className='img'/>
+
+				</section>
+
+				{/* <AktualnosciList /> */}
 
 				<section className="content-wrapper content-wrapper--center">
 
@@ -80,7 +92,7 @@ export default function Home() {
 					{
 						cardArr.map(card => (
 							<Card
-							key={card.id}
+								key={card.id}
 								title={card.title}
 								link={card.link}
 								image={card.image}
